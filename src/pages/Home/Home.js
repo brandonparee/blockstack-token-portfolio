3ebd73255038e875921163fa806eed811c1fe45f
Brandon Paree <brandon.paree@gmail.com>
2017-11-16 23:30:57 -0700

    Add Section component to Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import Section from '../../components/Bulma/Section'
+
 const mapStateToProps = ({}) => {
   return {}
 }
 
 const Home = ({}) => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Section title='Home'>
+    </Section>
   )
 }
 
