51dd7118144f843b87c123292026a28080696a4e
Brandon Paree <brandon.paree@gmail.com>
2017-12-01 19:26:07 -0700

    Improve LiveTradeData

--- src/components/Tables/LiveTradeData.js
+++ src/components/Tables/LiveTradeData.js
@@ -3,14 +3,14 @@ import ReactTable from 'react-table'
 import axios from 'axios'
 import io from 'socket.io-client'
 
-import LiveTradeDataTable from './LiveTradeDataTable'
+import Fiat from '../Helpers/Fiat'
 
 const cryptoCompare = `https://min-api.cryptocompare.com/data`
 const cryptoCompareWS = 'https://streamer.cryptocompare.com/'
-// const socket = io(cryptoCompareWS)
 
 class LiveTradeData extends Component {
-  state = { data: [] }
+  state = { data: [], subscriptions: [] }
+  socket = io(cryptoCompareWS)
 
   componentDidMount() {
     const fsym = this.props.token.toUpperCase()
@@ -22,41 +22,71 @@ class LiveTradeData extends Component {
     })
     .then((res) => {
       const subscriptions = res.data.USD.TRADES
-      // socket.emit('SubAdd', { subs: subscriptions })
-      // socket.on('m', (data) => this.handleSocketData(data))
+      this.setState({ subscriptions })
+      this.socket.emit('SubAdd', { subs: subscriptions })
+      this.socket.on('m', (data) => this.handleSocketData(data))
     })
     .catch((err) => {
       console.log(err)
     })
+  }
 
+  componentWillUnmount() {
+    this.socket.close()
   }
 
   handleSocketData(data) {
     const currentTrade = data.split('~')
     let type
-    console.log(currentTrade[4])
     if (currentTrade[4] === '1') {
       type = 'Buy'
     } else if (currentTrade[4] === '2') {
       type = 'Sell'
     }
 
-    const tradeData = {
-      type: type,
-      exchange: currentTrade[1],
-      quantity: currentTrade[7],
-      price: currentTrade[8],
-      total: currentTrade[9]
+    if (currentTrade[4] !== '4' && currentTrade[1] !== 'LOADCOMPLETE') {
+      const tradeData = {
+        type: type,
+        exchange: currentTrade[1],
+        quantity: currentTrade[7],
+        price: currentTrade[8],
+        total: currentTrade[9]
+      }
+      const trades = this.state.data.slice(0,14)
+      console.log(trades.length)
+      this.setState({ data: [tradeData, ...trades] })
     }
-
-    this.setState({ data: [tradeData] })
   }
 
   render() {
     const { token } = this.props
 
     return (
-      <LiveTradeDataTable data={this.state.data} />
+      <ReactTable
+        data={this.state.data}
+        noDataText='Live Trade Data Table coming soon'
+        columns={[
+          {
+            Header: 'Type',
+            accessor: 'type'
+          }, {
+            Header: 'Exchange',
+            accessor: 'exchange'
+          }, {
+            Header: 'Quanity',
+            accessor: 'quantity'
+          }, {
+            Header: 'Price',
+            accessor: 'price',
+            Cell: (row) => <Fiat value={row.value} />
+          }, {
+            Header: 'Total',
+            accessor: 'total',
+            Cell: (row) => <Fiat value={row.value} />
+          }
+        ]}
+        defaultPageSize={15}
+      />
     )
   }
 }
