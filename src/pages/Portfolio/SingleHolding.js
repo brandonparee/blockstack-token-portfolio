3ca5b4a908e475bf8e969d98de0546030c64e798
Brandon Paree <brandon.paree@gmail.com>
2017-12-03 04:30:19 -0700

    Display correct name on Portfolio page

--- src/pages/Portfolio/SingleHolding.js
+++ src/pages/Portfolio/SingleHolding.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import _ from 'lodash'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { portfolioLocalEdit, getConvertedPortfolio } from '../../actions/portfolioActions'
@@ -6,24 +7,26 @@ import { getTokenName, getFiatInfo, prettyFiat, prettyCrypto } from '../../utils
 
 import PrettyPercent from '../../components/Helpers/PrettyPercent'
 
-const mapStateToProps = ({portfolio, preferences}) => {
+const mapStateToProps = ({portfolio, preferences, marketData}) => {
   return {
     portfolio,
-    preferences
+    preferences,
+    coinList: marketData.coinList
   }
 }
 
-const SingleHolding = ({portfolio, preferences, abbreviation}) => {
+const SingleHolding = ({ portfolio, preferences, abbreviation, coinList }) => {
   const { convertedPortfolio } = portfolio
   const currentStats = convertedPortfolio[abbreviation] ? convertedPortfolio[abbreviation] : {amount: 0, fiatValue: 0, dayChange: 0, percentChange: 0}
   const fiatInfo = getFiatInfo(preferences.fiat)
+  const tokenInfo = coinList[abbreviation]
 
   return (
     <div className='SingleHolding'>
       <Link to={`/portfolio/${abbreviation.toLowerCase()}`}>
         <div className='box'>
           <div className='media-content'>
-            <p className='link-style'>Name...{abbreviation}</p>
+            <p className='link-style'>{tokenInfo.FullName}</p>
             <p>{`${prettyCrypto(currentStats.amount)}${abbreviation}`}</p>
             <p>{fiatInfo.symbol}{prettyFiat(currentStats.fiatValue)}</p>
             <PrettyPercent value={prettyFiat(currentStats.percentChange)} />
