c59b113ec6dab4bd0595a48066a7c808a82bb5ad
Brandon Paree <brandon.paree@gmail.com>
2017-12-11 00:27:13 -0700

    Update style for ErrorModal

--- src/components/Modals/ErrorModal.js
+++ src/components/Modals/ErrorModal.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Modal from 'react-modal'
 
+import { modalStyle } from './ModalStyle'
+
 const mapStateToProps = ({ file }) => {
   return {
     file
@@ -12,7 +14,8 @@ const ErrorModal = ({ file }) => {
   return (
     <Modal
       isOpen={!!(file.error && !file.fileSetup)}
-      ariaHideApp={false}>
+      ariaHideApp={false}
+      style={modalStyle} >
       <code>{file.error ? file.error.toString() : null}</code>
       <p className='is-text-4'>There was an issue loading data from Blockstack, please refresh the page and if necessary clear your cache.</p>
       <p className='is-text-4'>In some cases you may need to restart the Blockstack App.</p>
--- src/components/Modals/TransactionModal.js
+++ src/components/Modals/TransactionModal.js
@@ -3,31 +3,7 @@ import { connect } from 'react-redux'
 import Modal from 'react-modal'
 
 import { closeTransactionModal } from '../../actions/transactionActions'
-
-const modalStyle = {
-  overlay : {
-    position          : 'fixed',
-    top               : 0,
-    left              : 0,
-    right             : 0,
-    bottom            : 0,
-    backgroundColor   : 'rgba(255, 255, 255, 0.75)'
-  },
-  content : {
-    position                   : 'absolute',
-    top                        : '33%',
-    left                       : '33%',
-    right                      : '33%',
-    bottom                     : '33%',
-    border                     : '1px solid #ccc',
-    background                 : '#fff',
-    overflow                   : 'auto',
-    WebkitOverflowScrolling    : 'touch',
-    borderRadius               : '4px',
-    outline                    : 'none',
-    padding                    : '20px'
-  }
-}
+import { modalStyle } from './ModalStyle'
 
 const mapStateToProps = ({ transactions }) => {
   return {
@@ -39,7 +15,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     handleCloseButton: () => {
       dispatch(closeTransactionModal())
-    } 
+    }
   }
 }
 
