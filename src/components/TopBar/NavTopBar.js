69438b56d09e303b9bc67d3eebdcf60f4299dff0
Brandon Paree <brandon.paree@gmail.com>
2017-12-10 21:39:27 -0700

    Update logos and add logo to TopBar

--- src/components/TopBar/NavTopBar.js
+++ src/components/TopBar/NavTopBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import './NavTopBar.css'
+import logo from './Logo.svg'
 
 const NavTopBar = ({ user }) => {
   return (
     <div className='nav-top-bar NavTopBar'>
+      <img src={logo} className='logo' alt="logo"/>
       <h1 className='title'>peachy.</h1>
     </div>
   )
